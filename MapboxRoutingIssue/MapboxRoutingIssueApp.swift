30deec99ee3bd5772afddaabcbef251a1f799309
Michael Treacher <918794+treacher@users.noreply.github.com>
2023-07-24 14:10:31 +1000

    Initial Commit

---
+++ MapboxRoutingIssue/MapboxRoutingIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxRoutingIssueApp.swift
+//  MapboxRoutingIssue
+//
+//  Created by Michael Treacher on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapboxRoutingIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
